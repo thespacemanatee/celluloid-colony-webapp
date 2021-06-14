8dfff023853d46c7e456023765b01fbd0d48414d
Chee Kit <cheekit.chong98@gmail.com>
2021-06-15 03:29:29 +0800

    Try to fix mapbox transpile issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import DashboardMap from '../layouts/common/DashboardMap';
 import borders from '../data/indonesia-en.json';
+import mapboxgl from 'mapbox-gl';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass =
+  require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const IndexPage = () => {
   if (!borders) {
